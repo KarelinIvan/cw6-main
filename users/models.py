99fc65ef4b993fb6d16965301514a1736ca673a1
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-14 22:18:52 +0300

    commit 7-я правка

--- users/models.py
+++ users/models.py
@@ -8,6 +8,10 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
 
+    avatar = models.ImageField(upload_to="users/", verbose_name="аватар")
+    phone = models.CharField(max_length=35, verbose_name="номер телефона")
+    country = models.CharField(max_length=50, verbose_name="страна")
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
